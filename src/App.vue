a87353ef69ac7dd1d9b6b17af5c67f860c28d184
Igor Ponso <igorponso07@gmail.com>
2023-06-23 22:34:12 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+  import { useHead } from '@vueuse/head';
+
+  useHead({
+    title: '<your-project-name>',
+    meta: [{ name: 'description', content: 'Sample Example' }],
+  });
+</script>
+
+<template>
+  <router-view />
+</template>
+
+<style scoped></style>
---
+++ vite.config.ts
@@ -0,0 +1,63 @@
+import vue from '@vitejs/plugin-vue';
+import { defineConfig } from 'vite';
+import path from 'node:path';
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import Pages from 'vite-plugin-pages';
+import Inspect from 'vite-plugin-inspect';
+import Layouts from 'vite-plugin-vue-layouts';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      stylus: {
+        imports: [path.resolve(__dirname, './src/assets/stylus/*.styl')],
+      },
+    },
+  },
+  plugins: [
+    vue(),
+    VueI18nPlugin({
+      include: path.resolve(__dirname, './path/to/src/locales/**'),
+    }),
+    AutoImport({
+      dts: true,
+      imports: ['vue', '@vueuse/core', '@vueuse/head', 'vue-router'],
+    }),
+    Layouts({
+      layoutsDirs: 'src/layouts',
+      defaultLayout: 'default',
+    }),
+    Pages({
+      extensions: ['vue'],
+    }),
+    Components({
+      extensions: ['vue'],
+      include: [/\.vue$/, /\.vue\?vue/],
+      dirs: [
+        'src/components/LayoutCompositions',
+        'src/components',
+        'src/widgets',
+      ],
+      dts: true,
+      directoryAsNamespace: true,
+    }),
+    Inspect(),
+  ],
+  server: {
+    host: '0.0.0.0',
+    port: 8000,
+    open: false,
+    cors: true,
+  },
+  optimizeDeps: {
+    exclude: ['oh-vue-icons/icons'],
+  },
+  base: '/',
+});
